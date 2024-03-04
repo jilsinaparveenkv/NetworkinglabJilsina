dc6f05fcf5acc1d5266c270259ecb0bcec6dd2d0
jilsinaparveenkv <146071330+jilsinaparveenkv@users.noreply.github.com>
2024-03-04 16:05:17 +0530

    Add files via upload

---
+++ network3/arguments.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "total arguments:$#"
+echo "first arguments:$1"
+echo "second arguments:$2"
+
---
+++ network3/number.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "enter two numbers"
+read a b
+result=`expr $a + $b`
+echo "result:$result"
+
