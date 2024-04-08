b36c6654f5ec05b73864a1d7f8bbd1d52c4c8d47
jilsinaparveenkv <146071330+jilsinaparveenkv@users.noreply.github.com>
2024-04-08 14:22:21 +0530

    Add files via upload

---
+++ input array elements/arraylist.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+declare -a array
+echo "Enter the limit"
+read n
+echo "Enter the elements"
+for((i=0;i<n;i++))
+do
+	read element
+	array+=("$element")
+done
+echo "Elements of the array"
+for element in "${array[@]}"
+do
+	echo "$element"
+done
+
+
