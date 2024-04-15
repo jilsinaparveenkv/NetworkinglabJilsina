787c8b43a9e8d000dd7b79b6a59026cb03d40916
jilsinaparveenkv <146071330+jilsinaparveenkv@users.noreply.github.com>
2024-04-15 16:33:24 +0530

    Add files via upload

---
+++ factorialfunction/factorialfunction.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+fact(){
+	num=$1
+	fact=1
+	for((i=1;i<=num;i++));
+	do	
+		fact=$((fact*i))
+		
+	done
+	echo $fact
+}
+echo "Enter the number:"
+read number
+result=$(fact $number)
+echo "factorial of $number is $result"
+	
